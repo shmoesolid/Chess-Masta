312737848e8b58bf29368a5f14bddae626b51089
Shane Koehler <shane.koehler@gmail.com>
2020-09-07 02:26:37 -0500

    added some models

--- models/game.js
+++ models/game.js
@@ -1,21 +1,25 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const gameSchema = new Schema({
-    hostId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    hostColor: { type: Number }, // 0 = white, 1 = black
-    hostTimer: { type: Number }, // time left
+const gameSchema = new Schema(
+    {
+        hostId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        hostColor: { type: Number }, // 0 = white, 1 = black
+        hostTimer: { type: Number }, // time left
 
-    clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    clientColor: { type: Number }, // 0 = white, 1 = black
-    clientTimer: { type: Number }, // time left
+        clientId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        clientColor: { type: Number }, // 0 = white, 1 = black
+        clientTimer: { type: Number }, // time left
 
-    boardData: { type: String },
-    history: { type: String },
-    chat: { type: String },
-    gameTime: { type: Number },
-    status: { type: Number }, // 0=created (byself), 1=created (user joined), 2=playing (white turn), 
-});
+        boardData: { type: String }, // json
+        history: { type: String },
+        chat: { type: String },
+        
+        gameTime: { type: Number },
+        gameStatus: { type: Number }, // 0=created (byself), 1=created (user joined), 2=playing (white turn), 
+    }, 
+    { timestamps: true }
+);
 
 const Game = mongoose.model("Game", gameSchema);
 
