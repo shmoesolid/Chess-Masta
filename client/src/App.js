143ac1bc1b517280d91abd1597ee203c7399a4aa
Shane Koehler <shane.koehler@gmail.com>
2020-09-11 17:13:20 -0500

    added frontend board test to convert board to react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import ShanePageTest from "./pages/ShanePagetest";
 import './css/themes/board_green_gray.css';
 import './css/board.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <ShanePageTest />
     </div>
   );
 }
---
+++ client/src/pages/ShanePagetest.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ShaneBoard from "../components/ShaneBoard";
+
+function ShanePageTest()
+{
+    return(
+        <ShaneBoard />
+    );
+}
+
+export default ShanePageTest;
