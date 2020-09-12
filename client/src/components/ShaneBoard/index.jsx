f70b4ece8e7a878c304ff69513054f9eca7d48ae
Shane Koehler <shane.koehler@gmail.com>
2020-09-12 00:43:02 -0500

    working on client board test and interface with server side

--- client/src/components/ShaneBoard/index.jsx
+++ client/src/components/ShaneBoard/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import chesssk from "chesssk";
 
 function ShaneBoard()
 {
+    const tableChess = document.getElementById("board");
+    var blackOnBottom = false;
     var NUM_TO_LETTER = [ "a", "b", "c", "d", "e", "f", "g", "h" ];
     var cellSize = getComputedStyle(document.documentElement).getPropertyValue('--cell-size').slice(0, -2); // removes 'px'
+    var game = new chesssk();
+    console.log(game._grid);
     
     function floorBySize(num)
     {
@@ -12,37 +17,46 @@ function ShaneBoard()
     
     function showCoords(event)
     {
-      let tableChess = document.getElementById("board");
+      var x;
+      var y;
       
       // white on bottom
-      var x = event.pageX - tableChess.offsetLeft;
-      var y = Math.abs(event.pageY - tableChess.offsetHeight - tableChess.offsetTop);
+      if (!blackOnBottom)
+      {
+        x = event.pageX - tableChess.offsetLeft;
+        y = Math.abs(event.pageY - tableChess.offsetHeight - tableChess.offsetTop);
+      }
       
       // black on bottom
-      //var x = Math.abs(event.clientX - tableChess.offsetWidth - tableChess.offsetLeft);
-      //var y = event.clientY - tableChess.offsetTop;
-
+      else
+      {
+        x = Math.abs(event.clientX - tableChess.offsetWidth - tableChess.offsetLeft);
+        y = event.clientY - tableChess.offsetTop;
+      }
+      
       // get array indexes
       var chessCol = floorBySize(x);
       var chessRow = floorBySize(y);
       
-      // setup display vars
+      // setup display vars (DEBUG)
       var coords = "Actual: " + x + ", " + y
         + "<br />Array: [ " + chessCol + " ][ " + chessRow + " ]"
         + "<br />Notation: " + NUM_TO_LETTER[ chessCol ] + (chessRow + 1)
         + "<br />";
       
-      // display
+      // display (DEBUG)
       document.getElementById("coords").innerHTML = coords;
     }
 
-    function createBoard() {
+    function createBoard() 
+    {
         let table = [];
         for (let x=0; x<8; x++)
         {
             let children = [];
-            children.push(<td/>,<td/>,<td/>,<td/>,<td/>,<td/>,<td/>,<td/>);
-            table.push(<tr>{children}</tr>);
+            for (let y=0; y<8; y++)
+                children.push(<td key={"row"+y}/>);
+            table.push(<tr key={"col-"+x}>{children}</tr>);
         }
         return table;
     }
@@ -56,17 +70,17 @@ function ShaneBoard()
 
                     {/*board styling*/}
                     <table id="t01" >
-                        {createBoard()}
+                        <tbody>{createBoard()}</tbody>
                     </table>
 
                     {/*pieces*/}
-                    <img id='piece_bbc8' src='/assets/img/bb.gif' alt='bishop' style={{position:'absolute',top:'65px',left:'28px'}} />
+                    <img id='piece_bbc8' src='/assets/img/bb.gif' alt='bishop' style={{position:'absolute',top:'0px',left:'0px'}} />
 
                 </div>
             </div>
             
             {/*current mouse click coords*/}
-            <div id="coords" style={{color: 'white'}}></div>
+            <div id="coords" style={{color: 'white'}} />
         </div>
     );
 }
