ae63757d11a41bb51d3d5b8be2aca6652553e321
Journey Cruz <journey.cruz@gmail.com>
2020-09-15 15:03:11 -0500

    create header, sidenav, and hamburger components and insert them into the app

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ShanePageTest from "./pages/ShanePagetest";
 //import './css/themes/board_green_gray.css';
 import './css/board.css';
+import Header from "./components/Header";
+import SideNav from "./components/SideNav";
+import Toggle from "./components/Toggle";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <SideNav />
+      <Toggle />
       <ShanePageTest />
     </div>
   );
