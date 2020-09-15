69afec73580058c64741f010eb1afa02008e7803
Journey Cruz <journey.cruz@gmail.com>
2020-09-15 16:13:04 -0500

    Style navbar using component-styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,52 @@
-import React from "react"
-import "../css/ComponentStyles.css"
+import React from "react";
+import { Nav, Navbar, Form, FormControl } from "react-bootstrap";
+import styled from "styled-components";
+
+const Styles = styled.div`
+  .navbar {
+    background-color: #222;
+  }
+  a,
+  .navbar-nav,
+  .navbar-light .nav-link {
+    color: #9fffcb;
+    &:hover {
+      color: white;
+    }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9fffcb;
+    &:hover {
+      color: white;
+    }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
 
 const Header = () => {
-    return(
-        <div id="header-wrapper">
-            <h3>This is the Header</h3>
-        </div>
-    )
-}
+  return (
+    <Styles>
+      <Navbar expand="lg">
+        <Navbar.Brand href="/">Chess Masta</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Item>
+              <Nav.Link href="/">Link 1</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link href="/about">Link 2</Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Styles>
+  );
+};
 
-export default Header
+export default Header;
