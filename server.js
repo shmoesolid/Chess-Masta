0bdce9cb421fd9ebbeae115731c7fdc5d55a9c79
Journey Cruz <journey.cruz@gmail.com>
2020-09-16 20:10:43 -0500

    connect to mongoDB Atlas and save credentials as a dotenv variable

--- server.js
+++ server.js
@@ -2,21 +2,36 @@ const {
     createServer
 } = require('http');
 const express = require("express");
-//const path = require('path');
-const routes = require("./routes");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 const morgan = require('morgan');
 const compression = require('compression');
+
+const routes = require("./routes");
 
 const normalizPort = port => parseInt(port, 10);
 const PORT = normalizPort(process.env.PORT || 3001);
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 app.disable('x-powered-by');
 app.use(compression());
 app.use(morgan('common'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    },
+    (err) => {
+        if (err) throw err;
+        console.log("MongoDB connection established");
+    });
 
 if (process.env.NODE_ENV === "production")
     app.use(express.static("client/build"));
