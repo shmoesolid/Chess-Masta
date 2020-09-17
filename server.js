a763660c6cde3779ac68aee2c0a4ffe2ec42821b
Journey Cruz <journey.cruz@gmail.com>
2020-09-16 21:26:38 -0500

    re-structure server, create route validations for requests, and create authentication using jsonwebtoken

--- server.js
+++ server.js
@@ -4,17 +4,28 @@ const {
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 const morgan = require('morgan');
 const compression = require('compression');
+require("dotenv").config();
 
 const normalizPort = port => parseInt(port, 10);
 const PORT = normalizPort(process.env.PORT || 3001);
 
 const app = express();
+
+const server = createServer(app);
+
+server.listen(PORT, err => {
+    if (err) throw err;
+
+    console.log(`Server listening on port: ${PORT}`);
+});
+
+if (process.env.NODE_ENV === "production")
+    app.use(express.static("client/build"));
+
 app.use(express.json());
 app.use(cors());
-
 app.disable('x-powered-by');
 app.use(compression());
 app.use(morgan('common'));
@@ -22,24 +33,16 @@ app.use(express.urlencoded({
     extended: true
 }));
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    },
-    (err) => {
-        if (err) throw err;
-        console.log("MongoDB connection established");
-    });
-
-if (process.env.NODE_ENV === "production")
-    app.use(express.static("client/build"));
-
-const server = createServer(app);
-server.listen(PORT, err => {
-    if (err) throw err;
-
-    console.log(`Server listening on port: ${PORT}`);
-});
-
 // Add routes
 app.use("/users", require("./routes/userRouter"));
+
+// Mongoose ORM
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+},
+(err) => {
+    if (err) throw err;
+    console.log("MongoDB connection established");
+});
