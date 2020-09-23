747e072de4d5cc0cef7aa9d7be38db9c7c8cf7dd
Journey Cruz <journey.cruz@gmail.com>
2020-09-22 21:58:20 -0500

    remove auth middleware for tokenIsValid route and change set cookie back to "res.cookie"

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -92,7 +92,7 @@ router.post("/login", async (req, res) => {
     }, process.env.JWT_SECRET, {
       expiresIn: process.env.NODE_ENV !== 'production' ? '1d' : '7d',
     });
-    res.cookies('token', token, {
+    res.cookie('token', token, {
       expires: new Date(Date.now() + expiration),
       secure: process.env.NODE_ENV === 'production' ? true : false, // use https if in production
       httpOnly: true,
@@ -121,7 +121,7 @@ router.delete("/delete", auth, async (req, res) => {
   }
 });
 
-router.get("/tokenIsValid", auth, async (req, res) => {
+router.get("/tokenIsValid", async (req, res) => {
   try {
     const token = req.cookies.token || "";
     if (!token) return res.json(false);
