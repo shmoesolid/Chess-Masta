e594a47ce29a001db0c743052c08df300b96a846
Shane Koehler <shane.koehler@gmail.com>
2020-09-28 11:58:16 -0500

    added import redirect

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom";
 
 import Games from "./pages/Games";
 import Login from "./pages/Login";
